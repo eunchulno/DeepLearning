adbb6626d2c3e09cbde63c99734c903e61daefd8
eunchulno <shdmscjf2@gmail.com>
2017-04-17 17:05:09 +0900

    temp

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -75,10 +75,10 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, dtype=tf.float32))
 # 훈련
 sess.run(tf.global_variables_initializer())
 for i in range(20000):
-    batch = mnist.train.next_batch(50)
+    batch = mnist.train.next_batch(100)
     if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %f"%(i, train_accuracy))
+        print("step %d, training accuracy %0.04f"%(i, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 # 평가
 print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
